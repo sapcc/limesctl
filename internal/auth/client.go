ce0482ef79b01d512aa2eeb1648548a2088baefb
Muhammad Talal Anwar <talalanwar@outlook.com>
2019-11-11 11:01:06 +0100

    Remove superfluous nil checks

--- internal/auth/client.go
+++ internal/auth/client.go
@@ -5,8 +5,8 @@ import (
 
 	"github.com/gophercloud/gophercloud"
 	"github.com/gophercloud/gophercloud/openstack"
-	"github.com/gophercloud/utils/openstack/clientconfig"
 	"github.com/gophercloud/utils/client"
+	"github.com/gophercloud/utils/openstack/clientconfig"
 	"github.com/sapcc/gophercloud-limes/resources"
 	"github.com/sapcc/limesctl/internal/errors"
 )
@@ -15,28 +15,22 @@ import (
 // for Keystone and Limes.
 func ServiceClients(debug bool) (identityV3, limesV1 *gophercloud.ServiceClient) {
 	ao, err := clientconfig.AuthOptions(nil)
-	if err != nil {
-		errors.Handle(err, "can not get auth variables")
-	}
+	errors.Handle(err, "can not get auth variables")
 
 	provider, err := openstack.NewClient(ao.IdentityEndpoint)
-	if err != nil {
-		errors.Handle(err, "can not create an OpenStack client")
-	}
+	errors.Handle(err, "can not create an OpenStack client")
 
 	if debug {
 		provider.HTTPClient = http.Client{
 			Transport: &client.RoundTripper{
-				Rt: &http.Transport{},
+				Rt:     &http.Transport{},
 				Logger: &client.DefaultLogger{},
 			},
 		}
 	}
 
 	err = openstack.Authenticate(provider, *ao)
-	if err != nil {
-		errors.Handle(err, "can not connect to OpenStack")
-	}
+	errors.Handle(err, "can not connect to OpenStack")
 
 	identityClient, err := openstack.NewIdentityV3(provider, gophercloud.EndpointOpts{})
 	errors.Handle(err, "could not initialize identity client")
