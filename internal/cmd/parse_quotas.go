f592f22ceb5af0a427eb16b912e9b58b93afca1a
Muhammad Talal Anwar <muhammad.talal.anwar@sap.com>
2021-09-21 18:33:14 +0200

    Fix copyright notice
    Only the starting year is needed. The copyright notice is valid from the
    given year indefinitely.

--- internal/cmd/parse_quotas.go
+++ internal/cmd/parse_quotas.go
@@ -1,6 +1,6 @@
 /*******************************************************************************
 *
-* Copyright 2018-2020 SAP SE
+* Copyright 2018 SAP SE
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
